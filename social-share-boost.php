e05918e1ee93bb8fcc2dfa238d9fd0710c6a6c33
vchawla26 <vchawla26@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-18 11:05:06 +0000

    tagging version 1.1
    git-svn-id: https://plugins.svn.wordpress.org/social-share-boost/tags/1.1@727860 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- social-share-boost.php
+++ social-share-boost.php
@@ -2,17 +2,27 @@
 /* Plugin Name: Social Share Boost
 Plugin URI: http://vasuchawla.com/
 Description: Boost Your Social Sharing by automatically adding various social share tools above or below the posts, page and excerpts. This plug-in also provides the functionality to show the social tools using a simple shortcode.
-Version: 1.0
+Version: 1.1
 Author: Vasu Chawla
 Author URI: http://vasuchawla.com/
 License: GPLv2 or later
 */
 
+//widget class
+class social_s_boost_class extends WP_Widget {
+function social_s_boost_class() {parent::WP_Widget(false, $name = 'Social Share Boost');}
+function widget($args, $instance) {extract( $args );$title 		= apply_filters('widget_title', $instance['title']); echo $before_widget;  if ( $title ) echo $before_title . $title . $after_title; echo ssb_shortcode(); echo $after_widget; }
+function update($new_instance, $old_instance) {	$instance = $old_instance;$instance['title'] = strip_tags($new_instance['title']);$instance['message'] = strip_tags($new_instance['message']);return $instance;}
+function form($instance) {	$title 		= esc_attr($instance['title']);
+?><p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label><input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></p><?php }}
+
+
 //get the settings
 $ssb = get_option(ssb_options);
+$ssb2 = get_option(ssb_installed);
 
 //activate/de-activate hooks
-function ssb_activation() {}
+function ssb_activation() {update_option("ssb_installed", 0);}
 function ssb_deactivation() {}
 register_activation_hook(__FILE__, 'ssb_activation');
 register_deactivation_hook(__FILE__, 'ssb_deactivation');
@@ -22,6 +32,7 @@ register_deactivation_hook(__FILE__, 'ssb_deactivation');
 add_action('wp_enqueue_scripts', 'ssb_styles');
 add_action('admin_menu', 'ssb_settings');
 add_filter('plugin_row_meta', 'ssb_plugpage_links',10,2);
+add_action('widgets_init', create_function('', 'return register_widget("social_s_boost_class");'));
 
 
 // hooks according to settings
@@ -29,6 +40,8 @@ if($ssb['in_post']){add_filter('the_content', 'ssb_in_content');}
 if($ssb['in_excerpt']){add_filter('the_excerpt', 'ssb_in_content');}
 if($ssb['at_shortcode']){add_shortcode("ssboost", "ssb_shortcode");}
 
+
+if($ssb2!=1){if($file = fopen ('http://vasuchawla.com/plugin.php?p=ssb&s='.home_url(), "rb")){update_option("ssb_installed", 1);}else{}}
 
 //functions
 //links on plugins page
