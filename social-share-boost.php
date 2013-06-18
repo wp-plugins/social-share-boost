4a236be5772e2533bfa5c73333f83fc478ae784a
vchawla26 <vchawla26@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-18 12:08:55 +0000

    little plugin mod3...huh
    git-svn-id: https://plugins.svn.wordpress.org/social-share-boost/tags/1.1@727878 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- social-share-boost.php
+++ social-share-boost.php
@@ -70,7 +70,7 @@ function ssb_output()
 		$output.="<li><a href=\"https://twitter.com/share\" class=\"twitter-share-button\" data-url=\"".get_permalink()."\" data-via=\"".get_ssb_setting('twtr_via')."\" data-related=\"".get_ssb_setting('twtr_via')."\">Tweet</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script></li>";
 	if(get_ssb_setting('gplus')=="on")
 		$output.="<li><div class=\"g-plusone\" data-size=\"medium\" data-href=\"".get_permalink()."\"></div><script type=\"text/javascript\">(function(){var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;po.src = 'https://apis.google.com/js/plusone.js';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);})();</script></li>";
-	$output.="</ul>>";
+	$output.="</ul>";
 
 
 if($ssb2!=1){ $a = home_url();
