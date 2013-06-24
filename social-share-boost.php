2d13ca1a1ea1cfb7df9c9c1314618d198a206702
vchawla26 <vchawla26@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-24 15:21:50 +0000

    remove wrong characters creating header trouble
    git-svn-id: https://plugins.svn.wordpress.org/social-share-boost/tags/2.0@730944 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- social-share-boost.php
+++ social-share-boost.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 /* Plugin Name: Social Share Boost
 Plugin URI: http://vasuchawla.com/
 Description: Boost Your Social Sharing by automatically adding various social share tools above or below the posts, page and excerpts. This plug-in also provides the functionality to show the social tools using a simple shortcode.
