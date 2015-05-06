7fa937eca0b5d1a3b2d60cfa2b5f241cb2190992
SumoMe <SumoMe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-06 20:42:33 +0000

    fixing
    git-svn-id: https://plugins.svn.wordpress.org/social-share-boost/tags/4.3@1154846 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- social-share-boost.php
+++ social-share-boost.php
@@ -3,7 +3,7 @@
 Plugin URI: http://sumome.com/
 Description: Boost Your Social Sharing by automatically adding various social share tools above or below the posts, page and excerpts. This plug-in also provides the functionality to show the social tools using a simple shortcode.
 Version: 4.3
-Author: SumoMe
+Author: noahkagan
 Author URI: http://SumoMe.com/
 License: GPLv2 or later
 */
